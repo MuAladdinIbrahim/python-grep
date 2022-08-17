8373306675c0cf19c409f4c59d57d3a70047c8b5
Paul Kuruvilla <rohitpaulk@gmail.com>
2022-08-17 04:52:25 +0100

    Syncing with codecrafters-io/build-your-own-grep
    Created by https://github.com/codecrafters-io/course-definition-tester

---
+++ app/main.py
@@ -0,0 +1,25 @@
+import sys
+
+from dataclasses import dataclass
+
+# import pyparsing - available if you need it!
+# import lark - available if you need it!
+
+if sys.argv[1] != "-E":
+    print("Expected first argument to be '-E'")
+    exit(1)
+
+pattern = sys.argv[2]
+input = sys.stdin.read()
+
+# You can use print statements as follows for debugging, they'll be visible when running tests.
+print("Logs from your program will appear here!")
+
+if len(pattern) == 1:
+    # Uncomment this block to pass the first stage
+    # if input.count(pattern) > 0:
+    #     exit(0)
+    # else:
+    #     exit(1)
+else:
+    print(f"Unhandled pattern: {pattern}")
